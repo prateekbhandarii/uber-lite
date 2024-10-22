1785335c588eb96afc5752473e8f69f3c49fb9e8
Pratik Bhandari <pratik.bhandari.work@gmail.com>
2024-10-22 22:34:43 +0530

    initial commit

---
+++ src/test/java/com/spring/uber/UberLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
